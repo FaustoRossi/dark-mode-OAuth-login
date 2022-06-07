841238c6ed31cfe4aba895db77c6de441f68f580
Fausto Rossi <faustorossi@usal.edu.ar>
2022-06-07 00:32:13 -0300

    login with dark/light-mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import useLocalStorage from "use-local-storage";
+import { FcGoogle } from "react-icons/fc";
+import { BsToggleOn } from "react-icons/bs";
+
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light");
+
+	const toggleTheme = () => {
+    
+		const newTheme = theme === "light" ? "dark" : "light";
+		setTheme(newTheme);
+	};
+
+	return (
+		<div className="app" data-theme={theme}>
+			<div className="login">
+				<h1>Login</h1>
+				<div className="container">
+					<div className="top">
+            <p>Continue with Google</p>
+						<FcGoogle size={36} />
+					</div>
+					<p className="divider">
+						<span>Or</span>
+					</p>
+					<form>
+						<label htmlFor="email">Email</label>
+						<input type="text" name="email" placeholder="Enter your Email" />
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							name="password"
+							placeholder="Enter your password"
+						/>
+						<div className="remember">
+							<input type="checkbox" defaultChecked={true} />
+							<p>Remember Me</p>
+						</div>
+						<button>Log In</button>
+					</form>
+					<div className="bottom">
+						<p>Forget your password?</p>
+						<a href="/">Reset Password</a>
+					</div>
+					<p className="create">Create Account</p>
+				</div>
+				<div className="theme-toggle" >
+					<h2>Light Theme</h2>
+					<BsToggleOn size={32} onClick={toggleTheme} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
